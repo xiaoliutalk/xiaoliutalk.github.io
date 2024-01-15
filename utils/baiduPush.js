a3e88b02c93b7e8cbfba25ccfe7f435f5ee9d3bf
xiaoliu Talk <248920070@qq.com>
2024-01-15 14:34:47 +0800

    修改百度推送逻辑，避免推送次数过多影响配额

--- utils/baiduPush.js
+++ utils/baiduPush.js
@@ -11,7 +11,7 @@ const DOMAIN = process.argv.splice(2)[0]; // 获取命令行传入的参数
 
 let linkCount = 0;
 let currentUrlFileIndex = 1;
-const linksPerFile = 6;
+const linksPerFile = 10;
 
 if (DOMAIN) {
   main();
@@ -19,18 +19,17 @@ if (DOMAIN) {
   console.log(chalk.red('请在运行此文件时指定一个你要进行百度推送的域名参数，例：node utils/baiduPush.js https://www.xiaoliutalk.cn'))
 }
 
-
-
 /**
  * 主体函数
  */
 function main() {
-  // 检查并创建 urls 目录（如果不存在）
+  // 检查并创建 urls 目录
   if (!fs.existsSync(urlsRootDir)) {
     fs.mkdirSync(urlsRootDir, { recursive: true });
   }
-  let urlsFilePath = path.join(urlsRootDir, `urls_${currentUrlFileIndex}.txt`);
-  fs.writeFileSync(urlsFilePath, DOMAIN);
+
+  let currentUrlsFilePath = path.join(urlsRootDir, `urls_${currentUrlFileIndex}.txt`);
+  let linkCountInCurrentFile = 0;
 
   const files = readFileList(); // 读取所有md文件数据
 
@@ -38,16 +37,31 @@ function main() {
     const { data } = matter(fs.readFileSync(file.filePath, 'utf8'));
 
     if (data.permalink) {
-      const link = `\r\n${DOMAIN}${data.permalink}`;
-      console.log(link);
-      fs.appendFileSync(urlsFilePath, link);
+      const link = `${DOMAIN}${data.permalink}`;
 
-      linkCount++;
-      if (linkCount % linksPerFile === 0) {
+      console.log(link);
+
+      if (linkCountInCurrentFile === 0) {
+        fs.writeFileSync(currentUrlsFilePath, '');
+
+        // 如果有链接才增加计数
+        linkCount++;
+      }
+
+      fs.appendFileSync(currentUrlsFilePath, link);
+      linkCountInCurrentFile++;
+
+      if (linkCountInCurrentFile === linksPerFile || file === files[files.length - 1]) {
+        // 创建或切换到新的URLs文件
         currentUrlFileIndex++;
-        urlsFilePath = path.join(urlsRootDir, `urls_${currentUrlFileIndex}.txt`);
-        fs.writeFileSync(urlsFilePath, DOMAIN);
+        currentUrlsFilePath = path.join(urlsRootDir, `urls_${currentUrlFileIndex}.txt`);
+
+        // 重置当前文件内的链接计数
+        linkCountInCurrentFile = 0;
+      } else {
+        // 在非最后一行链接后面添加换行符
+        fs.appendFileSync(currentUrlsFilePath, '\r\n');
       }
     }
-  })
+  });
 }
